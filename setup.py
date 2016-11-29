9de8324987d32bb07e9f0e473fd010f052b9e24d
Alex Loosley <a.loosley@reply.de>
2016-11-29 17:10:05 +0100

    updated setup bugs, dependencies are now installed properly

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Alex Loosley <a.loosley@reply.de>"
+
 from setuptools import setup, find_packages
 
 setup(
-    name='python-highcharts_df-df',
+    name='python-highcharts_df',
     version='0.1.0',
     description='python-highcharts_df wrapper for customizable pretty plotting quickly from pandas dataframes',
     author="Alex Loosley",
@@ -9,7 +14,10 @@ setup(
     license='GNU',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=[],
+    install_requires=[
+        "colour>=0.1.2",
+        "python-highcharts>=0.3.0"
+    ],
     dependency_links=[],
     # include_package_data=True,  # should work, but doesn't, I think pip does not recognize git automatically
     package_data={
